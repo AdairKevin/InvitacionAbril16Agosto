367c14bce334158fd728010cd8b9997d9cfd0ef1
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2025-07-05 11:53:04 -0600

    cambios

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function abrirSobre() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const DATE_TARGET = new Date("08/02/2025 05:45 PM");
+  const DATE_TARGET = new Date("08/16/2025 04:00 PM");
 
   const SPAN_DAYS = document.querySelector("span#days");
   const SPAN_HOURS = document.querySelector("span#hours");
